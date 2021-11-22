028f0d927bf453bee7646c917c1c00d557f0d279
Johnny <johnny719top@gmail.com>
2021-11-22 23:29:54 +0800

    整理檔案

---
+++ src/components/HomePage/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BackgroundImage, Container, Wrapper } from './styles';
+
+const HomePage = () => {
+  return (
+    <>
+      <BackgroundImage />
+      <Wrapper>
+        <Container>
+          YO
+        </Container>
+      </Wrapper>
+    </>
+  );
+};
+
+export default HomePage;
---
+++ src/components/SearchPage/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BackgroundImage, Container, Wrapper } from './styles';
+
+const SearchPage = () => {
+  return (
+    <>
+      <BackgroundImage />
+      <Wrapper>
+        <Container>
+          Hey
+        </Container>
+      </Wrapper>
+    </>
+  );
+};
+
+export default SearchPage;
